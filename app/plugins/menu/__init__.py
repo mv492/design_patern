fd1c6e1f408988ae83ef024e0ab524b5c72a2e98
mv492 <mv492@njit.edu>
2025-02-25 22:50:33 -0500

    implemented four basic commands

--- app/plugins/menu/__init__.py
+++ app/plugins/menu/__init__.py
@@ -4,4 +4,11 @@ from app.commands import Command
 
 class MenuCommand(Command):
     def execute(self):
-        print(f'Menu')
+        print(f'add')
+        print(f'sub')
+        print(f'multiply')
+        print(f'divide')
+        print(f'goodbye')
+        print(f'greet')
+        print(f'email')
+        print(f'exit')
